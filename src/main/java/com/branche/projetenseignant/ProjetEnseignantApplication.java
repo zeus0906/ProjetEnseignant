2ae472714fe21fa99d441df3d592a1eff074caf4
zeus0906 <frankynyambi20@gmail.com>
2023-04-20 03:12:45 +0200

    Main

---
+++ src/main/java/com/branche/projetenseignant/ProjetEnseignantApplication.java
@@ -0,0 +1,13 @@
+package com.branche.projetenseignant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEnseignantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetEnseignantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/branche/projetenseignant/ProjetEnseignantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.branche.projetenseignant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetEnseignantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
